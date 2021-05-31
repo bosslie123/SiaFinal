d8a35363af55a1609ed84e6f0700ecc313cfdf0e
bosslie123 <bosslie123@gmail.com>
2021-05-31 09:31:36 +0800

    Commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <task v-bind:tasks="tasks"></task>
+  </div>
+</template>
+
+<script>
+import Task from "./components/Task";
+
+export default {
+  name: "App",
+  components: {
+    Task,
+  },
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: "Electric Fan",
+          completed: true,
+        },
+        {
+          id: 2,
+          title: "Rice Cooker",
+          completed: true,
+        },
+        {
+          id: 3,
+          title: "Pineapple Juice",
+          completed: false,
+        },
+        {
+          id: 4,
+          title: "Lumpia Wrapper",
+          completed: false,
+        },
+        {
+          id: 5,
+          title: "Vegetables",
+          completed: false,
+        },
+      ],
+    };
+  },
+};
+</script>
